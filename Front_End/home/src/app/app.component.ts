8acf380478465e4e7b77bd697e9fe751e63616b2
Aijaz Khan <aijaz-khan@hotmail.com>
2017-03-09 18:43:29 +0500

    Angular changes

--- Front_End/home/src/app/app.component.ts
+++ Front_End/home/src/app/app.component.ts
@@ -1,4 +1,32 @@
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app works!';
+// }
+//
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+// src/app/app.component.ts
+
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +35,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  books;
+
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/books.json')
+      .subscribe(res => this.books = res.json());
+  }
 }
